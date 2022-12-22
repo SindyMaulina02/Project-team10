552e1170b851f2c4739dc373d564c2f159a4e37c
Ichika354 <handa82624@gmail.com>
2022-12-22 10:56:30 +0700

    Dashboard

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+    $nama = "Admin";
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,129 +11,235 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mobile Game Store</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Dashboard Admin</title>
+    <link rel="stylesheet" href="./assets/app/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/icons/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./dist/css/index.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav id="home">
-        <div class="container">
-            <h1><a href="#home">Mobile Game Store</a></h1>
 
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#tentang">Check invoice</a></li>
-                <li><a href="#pendidikan">Join reseller</a></li>
-                <li><a href="#hoby">Join comunitas</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <button class="btn-login"><a href="#tentang">COME ON, FOLLOW ME!</a></button>
-            </ul>
+    <div class="wrapper">
+        <nav class="navbar navbar-expand-md navbar-light bg-light py-1">
+            <div class="container-fluid">
+                <button class="btn btn-default" id="btn-slider" type="button">
+                    <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
+                </button>
+                <a class="navbar-brand me-auto text-danger" href="#">Dash<span class="text-warning">Board</span></a>
+                <ul class="nav ms-auto">
+                    <li class="nav-item dropstart">
+                        <a class="nav-link text-dark ps-3" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown">
+                            <i class="fa fa-bell fa-lg py-2" aria-hidden="true"></i>
+                            <span class="badge bg-danger">10</span>
+                        </a>
+                        <div class="dropdown-menu mt-2 pt-0" aria-labelledby="navbarDropdown">
+                            <div class="d-flex p-3 border-bottom align-items-cente mb-2">
+                                <i class="fa fa-bell me-3" aria-hidden="true"></i>
+                                <span class="fw-bold lh-1">Notifikasi</span>
+                            </div>
+                            <a class="dropdown-item py-2" href="#">
+                                <div class="d-flex overflow-hidden">
+                                    <i class="fa fa-envelope fa-lg dropdown-icons bg-primary text-white p-2 me-2"
+                                        aria-hidden="true"></i>
+                                    <div class="d-block content">
+                                        <p class="lh-1 mb-0">Safna Prasetiono</p>
+                                        <span class="content-text">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit. Quia sint laboriosam in architecto earum.</span>
+                                    </div>
+                                </div>
+                            </a>
+                            <a class="dropdown-item py-2" href="#">
+                                <div class="d-flex overflow-hidden">
+                                    <i class="fa fa-file fa-lg dropdown-icons bg-warning text-white p-2 me-2"
+                                        aria-hidden="true"></i>
+                                    <div class="d-block content">
+                                        <p class="lh-1 mb-0">file informations</p>
+                                        <span class="content-text">your file has exceeded the limit.</span>
+                                    </div>
+                                </div>
+                            </a>
+                            <a class="dropdown-item py-2" href="#">
+                                <div class="d-flex overflow-hidden">
+                                    <i class="fa fa-hdd-o fa-lg dropdown-icons bg-danger text-white p-2 me-2"
+                                        aria-hidden="true"></i>
+                                    <div class="d-block content">
+                                        <p class="lh-1 mb-0">Storage</p>
+                                        <span class="content-text">Your storage is full, delete some files.</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropstart">
+                        <a class="nav-link text-dark ps-3 pe-1" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown">
+                            <img src="./images/user/user.png" alt="user" class="img-user">
+                        </a>
+                        <div class="dropdown-menu mt-2 pt-0" aria-labelledby="navbarDropdown">
+                            <div class="d-flex p-3 border-bottom mb-2">
+                                <img src="./images/user/user.png" alt="user" class="img-user me-2">
+                                <div class="d-block">
+                                    <p class="fw-bold m-0 lh-1">YourName</p>
+                                    <small>YourAccount@gmail.com</small>
+                                </div>
+                            </div>
+                            <a class="dropdown-item" href="#">
+                                <i class="fa fa-user fa-lg me-3" aria-hidden="true"></i>Profile
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                <i class="fa fa-cog fa-lg me-3" aria-hidden="true"></i>Setting
+                            </a>
+                            <hr class="dropdown-divider">
+                            <a class="dropdown-item" href="#">
+                                <i class="fa fa-sign-out fa-lg me-2" aria-hidden="true"></i>LogOut
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="slider bg-primary" id="sliders">
+            <div class="slider-head">
+                <div class="d-block pt-4 pb-3 px-3">
+                    <img src="./images/user/user.png" alt="user" class="slider-img-user mb-2">
+                    <p class="fw-bold mb-0 lh-1 text-white"><?= $nama; ?></p>
+                    <small class="text-white">Admin@yahoo.com</small>
+                </div>
+            </div>
+            <div class="slider-body px-1">
+                <nav class="nav flex-column">
+                    <a class="nav-link px-3 active" href="#">
+                        <i class="fa fa-home fa-lg box-icon" aria-hidden="true"></i>Home
+                    </a>
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-user fa-lg box-icon" aria-hidden="true"></i>Profile
+                    </a>
+                    <hr class="soft my-1 bg-white">
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-dropbox fa-lg box-icon" aria-hidden="true"></i>Product
+                    </a>
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-calendar fa-lg box-icon" aria-hidden="true"></i>Pleanning
+                    </a>
+                    <hr class="soft my-1 bg-white">
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-bell fa-lg box-icon" aria-hidden="true"></i>Notifikasi
+                    </a>
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-envelope fa-lg box-icon" aria-hidden="true"></i>Message
+                    </a>
+                    <hr class="soft my-1 bg-white">
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-id-card fa-lg box-icon" aria-hidden="true"></i>License
+                    </a>
+                    <hr class="soft my-1 bg-white">
+                    <a class="nav-link px-3" href="#">
+                        <i class="fa fa-sign-out fa-lg box-icon" aria-hidden="true"></i>LogOut
+                    </a>
+                </nav>
+            </div>
         </div>
-    </nav>
-    <div class="container_section">
-        <section id="#home" class="image_2">
-            <img src="fotoprofile.JPG" alt="">
-            <h1>SEKOLAH ALAM</h1>
-            <p>Sekolah Alam Cendrawasih</p>
-            <h4>Learning Fun For Us</h4>
-            <button class="btn-come"><a href="#tentang">COME ON, FOLLOW ME!</a></button>
-        </section>
-    </div>
-    <div class="tentang">
-        <section id="tentang">
-            <div class="container_tentang">
-                <div class="tentang_3">
-                    <h1>Sekolah Alam Cendrawasih</h1>
-                    <p>Hello everyone! Kenalin aku Melani Isnaini, kalian bisa panggil aku Mel atau Melani. Aku
-                        dilahirkan
-                        pada tanggal 02 Mei 2004 sekaligus dibesarkan di Kota Hijau yaitu Kota Cimahi, tepatnya di Kota
-                        Cimahi Utara, Kecamatan Cibabat. Hobiku hanya mendengarkan lagu, yaa selain itu menggambar jika
-                        aku
-                        ada waktu luang atau sedang suntuk. Oh iyaa aku juga suka nonton film, terutama genre film
-                        horror
-                        dan goreee. Apa lagi yaaa??? Bingung… ah iyaa riwayat pendidikan aku yaa, pertama SDN Cibabat
-                        Mandiri 5, lalu SMPN 10 Cimahi, terakhir SMAN 3 Cimahi. Lanjuttt, sekarang aku melanjutkan
-                        pendidikanku di Universitas Logistik dan Bisnis Internasional prodi Teknik Informatika D4. Ahh
-                        cape
-                        juga ya aku ngejelasin segini, padahal ga terlalu panjang… okay, selebihnya bisa diliat di slide
-                        berikutnyaa! :3</p>
-                </div>
-                <div class="image_12">
-                    <img src="IMG_1520.HEIC" alt="">
-                </div>
-            </div>
-        </section>
 
-    </div>
-    <div class="extraculliculer">
-        <section id="hobi">
-            <div class="kolom">
-                <div class="deskripsi">
-                    <h1>HOBI</h1>
-                </div>
-                <div class="ukm-list">
-                    <div class="kartu-ukm">
-                        <img src="WhatsApp Image 2022-10-19 at 23.57.49.jpeg" alt="">
-                        <p>drawing</p>
-                    </div>
-                    <div class="kartu-ukm">
-                        <img src="WhatsApp Image 2022-10-19 at 23.57.48.jpeg" alt="">
-                        <p>music</p>
+        <div class="main-pages">
+            <div class="container-fluid">
+                <div class="row g-2 mb-3">
+                    <div class="col-12">
+                        <div class="d-block bg-white rounded shadow p-3">
+                            <h2>Hallo <?= $nama; ?></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum labore maiores facilis
+                                optio illo tempora quod omnis veniam dolores. Est porro omnis, quae numquam velit
+                                accusantium perferendis inventore sint consectetur.</p>
+                        </div>
                     </div>
                 </div>
 
-            </div>
-
-
-        </section>
-
-    </div>
-    <div class="study">
-        <section id="pendidikan">
-            <div class="pendidikan">
-                <div class="pendidikan_deskripsi">
-                    <h1>PENDIDIKAN</h1>
+                <div class="row g-3 mb-3">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="card p-2 shadow">
+                            <div class="d-flex align-items-center px-2">
+                                <i class="fa fa-plane float-start fa-3x py-auto" aria-hidden="true"></i>
+                                <div class="card-body text-end">
+                                    <h5 class="card-title">122</h5>
+                                    <p class="card-text">Pleanning</p>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-white">
+                                <small class="text-start fw-bold">Your Planning</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="card p-2 shadow">
+                            <div class="d-flex align-items-center px-2">
+                                <i class="fa fa-money fa-3x py-auto" aria-hidden="true"></i>
+                                <div class="card-body text-end">
+                                    <h5 class="card-title">$ 123.05</h5>
+                                    <p class="card-text">Money</p>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-white">
+                                <small class="text-start fw-bold">Your Money</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="card p-2 shadow">
+                            <div class="d-flex align-items-center px-2">
+                                <i class="fa fa-calendar float-start fa-3x py-auto" aria-hidden="true"></i>
+                                <div class="card-body text-end">
+                                    <h5 class="card-title">122</h5>
+                                    <p class="card-text">Pleanning</p>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-white">
+                                <small class="text-start fw-bold">Your Schedule</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+                        <div class="card p-2 shadow">
+                            <div class="d-flex align-items-center px-2">
+                                <i class="fa fa-users float-start fa-3x py-auto" aria-hidden="true"></i>
+                                <div class="card-body text-end">
+                                    <h5 class="card-title">122</h5>
+                                    <p class="card-text">Customer</p>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-white">
+                                <small class="text-start fw-bold">Your Customer</small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="pendidikan_list">
-                    <div class="kuliah_list">
-                        <p>SD NEGERI CIBABAT MANDIRI 5 adalah salah satu satuan pendidikan dengan jenjang SD di Cibabat,
-                            Kec. Cimahi Utara, Kota Cimahi, Jawa Barat. Dalam menjalankan kegiatannya, SD NEGERI CIBABAT
-                            MANDIRI 5 berada di bawah naungan Kementerian Pendidikan dan Kebudayaan.</p>
-                        <img src="WhatsApp Image 2022-10-19 at 23.57.44.jpeg" alt="">
-                    </div>
-                    <div class="kuliah_list">
-                        <p>SMP NEGERI 10 CIMAHI adalah salah satu satuan pendidikan dengan jenjang SMP di Cibabat, Kec.
-                            Cimahi Utara, Kota Cimahi, Jawa Barat. Dalam menjalankan kegiatannya, SMP NEGERI 10 CIMAHI
-                            berada di bawah naungan Kementerian Pendidikan dan Kebudayaan.</p>
-                        <img src="WhatsApp Image 2022-10-19 at 23.57.42.jpeg" alt="">
-                    </div>
-                    <div class="kuliah_list">
-                        <p>SMAN 3 Cimahi adalah sekolah negeri dengan akreditasi A yang terletak di Jalan Pesantren No.
-                            161, Kelurahan Cibabat, Kecamatan Cimahi Utara, Kota Cimahi yang sekarang dikepalai oleh
-                            Kepala Sekolah Bapak Drs. H. Yayat Supriyat, M.M.Pd.</p>
-                        <img src="WhatsApp Image 2022-10-19 at 23.57.45 (1).jpeg" alt="">
-                    </div>
-                </div>
 
-            </div>
-        </section>
-
-    </div>
-<div id="contact">
-        <div class="wrapper">
-            <div class="footer">
-                <div class="footer-section">
-                    <h3>Contact Me!</h3>
-                    <p>Email:<a href="">melanisnaini@gmail.com</a></p>
-                    <p>WhatsApp : 085864434470</p>
-
+                <div class="row g-2">
+                    <div class="col-12 col-lg-6">
+                        <div class="d-block rounded shadow bg-white p-3">
+                            <canvas id="myChartOne"></canvas>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <div class="d-block rounded shadow bg-white p-3">
+                            <canvas id="myChartTwo"></canvas>
+                        </div>
+                    </div>
                 </div>
 
             </div>
-
         </div>
-
     </div>
+
+    <div class="slider-background" id="sliders-background"></div>
+    <script src="./dist/js/jquery.js"></script>
+    <script src="./assets/app/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+    <script src="./dist/js/index.js"></script>
 
 </body>
 
