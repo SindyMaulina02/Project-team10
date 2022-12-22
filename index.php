22df432e2ae272419b1f18f92526cf0eed522be1
SindyMaulina02 <maulinasindy6@gmail.com>
2022-12-22 10:11:32 +0700

    Tampilan

--- index.php
+++ index.php
@@ -1,12 +1,134 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mobile Game Store</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <nav id="home">
+        <div class="container">
+            <h1><a href="#home">Mobile Game Store</a></h1>
+
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#tentang">Check invoice</a></li>
+                <li><a href="#pendidikan">Join reseller</a></li>
+                <li><a href="#hoby">Join comunitas</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <button class="btn-login"><a href="#tentang">COME ON, FOLLOW ME!</a></button>
+            </ul>
+        </div>
+    </nav>
+    <div class="container_section">
+        <section id="#home" class="image_2">
+            <img src="fotoprofile.JPG" alt="">
+            <h1>SEKOLAH ALAM</h1>
+            <p>Sekolah Alam Cendrawasih</p>
+            <h4>Learning Fun For Us</h4>
+            <button class="btn-come"><a href="#tentang">COME ON, FOLLOW ME!</a></button>
+        </section>
+    </div>
+    <div class="tentang">
+        <section id="tentang">
+            <div class="container_tentang">
+                <div class="tentang_3">
+                    <h1>Sekolah Alam Cendrawasih</h1>
+                    <p>Hello everyone! Kenalin aku Melani Isnaini, kalian bisa panggil aku Mel atau Melani. Aku
+                        dilahirkan
+                        pada tanggal 02 Mei 2004 sekaligus dibesarkan di Kota Hijau yaitu Kota Cimahi, tepatnya di Kota
+                        Cimahi Utara, Kecamatan Cibabat. Hobiku hanya mendengarkan lagu, yaa selain itu menggambar jika
+                        aku
+                        ada waktu luang atau sedang suntuk. Oh iyaa aku juga suka nonton film, terutama genre film
+                        horror
+                        dan goreee. Apa lagi yaaa??? Bingung… ah iyaa riwayat pendidikan aku yaa, pertama SDN Cibabat
+                        Mandiri 5, lalu SMPN 10 Cimahi, terakhir SMAN 3 Cimahi. Lanjuttt, sekarang aku melanjutkan
+                        pendidikanku di Universitas Logistik dan Bisnis Internasional prodi Teknik Informatika D4. Ahh
+                        cape
+                        juga ya aku ngejelasin segini, padahal ga terlalu panjang… okay, selebihnya bisa diliat di slide
+                        berikutnyaa! :3</p>
+                </div>
+                <div class="image_12">
+                    <img src="IMG_1520.HEIC" alt="">
+                </div>
+            </div>
+        </section>
+
+    </div>
+    <div class="extraculliculer">
+        <section id="hobi">
+            <div class="kolom">
+                <div class="deskripsi">
+                    <h1>HOBI</h1>
+                </div>
+                <div class="ukm-list">
+                    <div class="kartu-ukm">
+                        <img src="WhatsApp Image 2022-10-19 at 23.57.49.jpeg" alt="">
+                        <p>drawing</p>
+                    </div>
+                    <div class="kartu-ukm">
+                        <img src="WhatsApp Image 2022-10-19 at 23.57.48.jpeg" alt="">
+                        <p>music</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </section>
+
+    </div>
+    <div class="study">
+        <section id="pendidikan">
+            <div class="pendidikan">
+                <div class="pendidikan_deskripsi">
+                    <h1>PENDIDIKAN</h1>
+                </div>
+                <div class="pendidikan_list">
+                    <div class="kuliah_list">
+                        <p>SD NEGERI CIBABAT MANDIRI 5 adalah salah satu satuan pendidikan dengan jenjang SD di Cibabat,
+                            Kec. Cimahi Utara, Kota Cimahi, Jawa Barat. Dalam menjalankan kegiatannya, SD NEGERI CIBABAT
+                            MANDIRI 5 berada di bawah naungan Kementerian Pendidikan dan Kebudayaan.</p>
+                        <img src="WhatsApp Image 2022-10-19 at 23.57.44.jpeg" alt="">
+                    </div>
+                    <div class="kuliah_list">
+                        <p>SMP NEGERI 10 CIMAHI adalah salah satu satuan pendidikan dengan jenjang SMP di Cibabat, Kec.
+                            Cimahi Utara, Kota Cimahi, Jawa Barat. Dalam menjalankan kegiatannya, SMP NEGERI 10 CIMAHI
+                            berada di bawah naungan Kementerian Pendidikan dan Kebudayaan.</p>
+                        <img src="WhatsApp Image 2022-10-19 at 23.57.42.jpeg" alt="">
+                    </div>
+                    <div class="kuliah_list">
+                        <p>SMAN 3 Cimahi adalah sekolah negeri dengan akreditasi A yang terletak di Jalan Pesantren No.
+                            161, Kelurahan Cibabat, Kecamatan Cimahi Utara, Kota Cimahi yang sekarang dikepalai oleh
+                            Kepala Sekolah Bapak Drs. H. Yayat Supriyat, M.M.Pd.</p>
+                        <img src="WhatsApp Image 2022-10-19 at 23.57.45 (1).jpeg" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    </div>
+<div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>Contact Me!</h3>
+                    <p>Email:<a href="">melanisnaini@gmail.com</a></p>
+                    <p>WhatsApp : 085864434470</p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </body>
+
 </html>
