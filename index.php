c832927601d762eded76b94b3d815914f25ee8ab
SindyMaulina02 <maulinasindy6@gmail.com>
2022-12-27 12:38:50 +0700

    Try

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    
+
 
 <head>
     <meta charset="UTF-8">
@@ -16,124 +16,147 @@
     <nav id="home">
         <div class="container">
             <h1><a href="#home">Mobile Game Store</a></h1>
-              <div></div>
+            <div></div>
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#tentang">Produk</a></li>
                 <li><a href="http://wa.me/6295379114998">Join reseller</a></li>
                 <li><a href="#joincomunitas">Join comunitas</a></li>
-                <li><a href="contactus.html">Contact Us</a></li>
+                <li><a href="contactUs1.php">Contact Us</a></li>
                 <button class="btn-login"><a href="#tentang">Login</a></button>
             </ul>
         </div>
     </nav>
     <section id="home">
-    <div class="container_section">
-        <section id="#home" class="image_2">
-            
-    </div>
-</section>
+        <div class="container_section">
+            <section id="#home" class="image_2">
 
-<div id="contact">
-    <div class="tentang">
-    <div class="container text-center">
-        
-        <div class="link">
-            <a href="#harga-ff">
-            <h2>Game popular<h2>
-            <div class="produk">
-        <div class="row row-cols-auto">
-          <div class="col"><img src="img/ff.png" alt=""><div class="judul-produk">Free Fire</div></div>
-          <div class="col"><img src="img/images/pubgm.jpg" alt=""><div class="judul-produk">Free Fire</div></div> 
-          <div class="col"><img src="img/images/valorant.jpg" alt=""><div class="judul-produk">Free Fire</div></div> 
-          <div class="col"><img src="img/images/mlbb.jpg" alt=""><div class="judul-produk">Free Fire</div></div>
-          <div class="col"><img src="img/images/higgs_domino.jpg" alt=""><div class="judul-produk">Free Fire</div></div> 
-          <div class="col"><img src="img/images/higgs_domino.jpg" alt=""><div class="judul-produk">Free Fire</div></div> 
-          
-        
         </div>
-    </div>   
-        <div class="overlay" id="harga-ff">
+    </section>
 
-            <div class="popup">
-                <h2>HARGA FREE FIRE</h2>
-                <a class="close" href="#populer">&times;</a>
-                <br>
-                <div class="content">
-                    70 Diamonds  : 
-                    <br>    
-                    <span class="harga">Rp 10.000</span>
-                    <br>
-                    140 Diamonds : 
-                    <br>
-                    <span class="harga">Rp 20.000</span>
-                    <br>
-                    210 Diamonds : 
-                    <br>
-                    <span class="harga">Rp 30.000</span>
-                    <br>
-                    280 Diamonds : 
-                    <br>
-                    <span class="harga">Rp 40.000</span>
-                    <br>
-                    355 Diamonds : 
-                    <br>
-                    <span class="harga">Rp 50.000</span>
-                    <br>
-                    500 Diamond  : 
-                    <br>
-                    <span class="harga">Rp 70.000</span>
-                    <br>
-                    720 Diamonds : 
-                    <br>
-                    <span class="harga">Rp 100.000</span>
-                    <br>
-                    1000 Diamonds : 
-                    <br>
-                    <span class="harga">Rp 130.000</span>
-                    <br>
-                    M. Mingguan  : 
-                    <br>
-                    <span class="harga">Rp 30.000</span>
-                    <br>
-                    M. Bulanan   : 
-                    <br>
-                    <span class="harga">Rp 150.000</span>
-                    <br>
-                    <a class="pesan" href="https://wa.me/6282132873886" 
-                    target="_blank" 
-                    rel="noopener noreferrer"><img src="./images/whatsapp.png" alt="">Pesan Sekarang</a>
-                    </div>
-                    </div>
-            
-            </div>
-        </div>
-        <div id="contact">
-            <div class="wrapper">
-                <div class="footer">
-                    <div class="footer-section">
-                        <h3>Mobile Game Store</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>
-                    </div>
-                    <div class="footer-section">
-                        <h3>About</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, culpa!</p>
-                    </div>
-                    <div class="footer-section">
-                        <h3>Contact</h3>
-                        <p></p>
+    <div id="tentang">
+        <div class="tentang">
+            <div class="container text-center">
+
+                <div class="link">
+                    <a href="#harga-ff">
+                        <h2>Game popular<h2>
+                                <div class="produk">
+                                    <div class="row row-cols-auto">
+                                        <div class="col"><img src="img/ff.png" alt="">
+                                            <div class="judul-produk">Free Fire</div>
+                                        </div>
+                                        <div class="col"><img src="img/pubgm.jpg" alt="">
+                                            <div class="judul-produk">Free Fire</div>
+                                        </div>
+                                        <div class="col"><img src="img/valorant.jpg" alt="">
+                                            <div class="judul-produk">Free Fire</div>
+                                        </div>
+                                        <div class="col"><img src="img/aov.jpg" alt="">
+                                            <div class="judul-produk">Free Fire</div>
+                                        </div>
+                                        <div class="col"><img src="img/GenshinImpact.jpg" alt="">
+                                            <div class="judul-produk">Free Fire</div>
+                                        </div>
+                                        <div class="col"><img src="img/higgs_domino.jpg" alt="">
+                                            <div class="judul-produk">Free Fire</div>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                                <div class="overlay" id="harga-ff">
+
+                                    <div class="popup">
+                                        <h2>HARGA FREE FIRE</h2>
+                                        <a class="close" href="#populer">&times;</a>
+                                        <br>
+                                        <div class="content">
+                                            70 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 10.000</span>
+                                            <br>
+                                            140 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 20.000</span>
+                                            <br>
+                                            210 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 30.000</span>
+                                            <br>
+                                            280 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 40.000</span>
+                                            <br>
+                                            355 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 50.000</span>
+                                            <br>
+                                            500 Diamond :
+                                            <br>
+                                            <span class="harga">Rp 70.000</span>
+                                            <br>
+                                            720 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 100.000</span>
+                                            <br>
+                                            1000 Diamonds :
+                                            <br>
+                                            <span class="harga">Rp 130.000</span>
+                                            <br>
+                                            M. Mingguan :
+                                            <br>
+                                            <span class="harga">Rp 30.000</span>
+                                            <br>
+                                            M. Bulanan :
+                                            <br>
+                                            <span class="harga">Rp 150.000</span>
+                                            <br>
+                                            <a class="pesan" href="http://wa.me/6295379114998" target="_blank"
+                                                rel="noopener noreferrer"><img src="./images/whatsapp.png" alt="">Pesan
+                                                Sekarang</a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                </div>
+                <div id="contact">
+                    <div class="wrapper">
+                        <div class="footer">
+                            <div class="footer-section">
+                                <h3>Mobile Game Store</h3>
+                                <p>Mobilegamestore.id adalah Platform webisite top up game termurah, tercepat, dan
+                                    terpercaya di Indonesia. Proses cepat membuat Anda nyaman Top Up disini. Dengan
+                                    Menyediakan berbagai macam pembayaran Transfer Bank,
+                                    E-Wallet, Scan QR, Alfamart, & Indomart dan pasti memudahkan anda untuk Top Up akun
+                                    games kalian.</p>
+                            </div>
+                            <div class="footer-section">
+                                <h3>Menu</h3>
+                                <ul>
+                                    <li><a href="#home">Home</a></li>
+                                    <li><a href="#tentang">Produk</a></li>
+                                    <li><a href="http://wa.me/6295379114998">Join reseller</a></li>
+                                    <li><a href="#joincomunitas">Join comunitas</a></li>
+                                    <li><a href="contactus.html">Contact Us</a></li>
+                                    <button class="btn-login"><a href="#tentang">Login</a></button>
+                                </ul>
+                            </div>
+                            <div class="footer-section">
+                                <h3>Other Services</h3>
+                                <p></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
-    
     </div>
-</div>
-</div>
-</section>
-      </div>
-            </div>
-        </div>
+    </section>
+    </div>
+    </div>
+    </div>
     </div>
 
 </body>
